908b1da48004f9f55f7108aefc9937e93b111d6f
Sameer Gaikwad <sameergaikwad.delxn@gmail.com>
2026-01-19 15:15:36 +0530

    sample change

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -23,7 +23,7 @@ export async function POST(req: Request) {
                 pass: process.env.EMAIL_PASS,
             },
         });
-
+//sample
         try {
             await transporter.verify();
         } catch (verifyError) {
